b3caee37a832a6ac546deb2be61afc0f606401c7
duncanharris12 <61689105+duncanharris12@users.noreply.github.com>
2022-06-09 17:56:32 +0200

    Onele Tomson

---
+++ src/test/java/za/ac/cput/Factory/MedicationFactoryTest.java
@@ -0,0 +1,25 @@
+package za.ac.cput.Factory;
+
+/* MedicationFactoryTest.java
+ Entity for the MedicationFactoryTest
+ Author: Onele Tomson (219483280)
+ Date: 08 May 2022
+*/
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api;
+import za.ac.cput.Entity.Medication;
+
+
+public class MedicationFactoryTest
+{
+    @Test
+    void createMedication() {
+    }
+    Medication medication = MedicationFactory.createMedication
+            ("122001", "Adcodol");
+    assertNotNull(medication);
+        System.out.println(medication);
+}
+
+}
