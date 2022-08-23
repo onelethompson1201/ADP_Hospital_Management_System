99bcb83bb0b2780b7381ebadf5cccb8b96662a4b
Nolubabalo Ndongeni <nndongeni89@gmail.com>
2022-08-23 21:40:40 +0200

    Test cases for Patient and TestPatient

---
+++ src/test/java/za/ac/cput/Controllers/TestPatientControllerTest.java
@@ -0,0 +1,29 @@
+package za.ac.cput.Controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestPatientControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
