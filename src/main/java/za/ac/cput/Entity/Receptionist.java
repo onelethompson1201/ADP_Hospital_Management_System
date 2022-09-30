4c47b4d4d3702b8c175d7ffa07f8bdec37fd2ddf
DavinC9903 <220021333@mycput.ac.za>
2022-09-30 21:56:47 +0200

    Updated Work From Davin

--- src/main/java/za/ac/cput/Entity/Receptionist.java
+++ src/main/java/za/ac/cput/Entity/Receptionist.java
@@ -9,9 +9,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 @Entity
-public class Receptionist
+public class Receptionist implements Serializable
 {
     @Id
     @Column(name = "receptionist_id")
--- src/main/java/za/ac/cput/Repository/IReceptionistRepository.java
+++ src/main/java/za/ac/cput/Repository/IReceptionistRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.Entity.Receptionist;
 
+import java.util.List;
 import java.util.Set;
 @Repository
 public interface IReceptionistRepository extends JpaRepository<Receptionist, String>
 {
-    public Set<Receptionist> getAll();
+    @Override
+    List<Receptionist> findAllById(Iterable<String> strings);
 }
