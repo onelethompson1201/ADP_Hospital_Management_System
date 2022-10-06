6a0f606c579a4b0cea46f81baff678247c5df700
NolubabaloNdongeni <nndongeni89@gmail.com>
2022-10-06 22:46:18 +0200

    Changes made

--- src/main/java/za/ac/cput/Security/SecurityConfig.java
+++ src/main/java/za/ac/cput/Security/SecurityConfig.java
@@ -1,16 +1,19 @@
 package za.ac.cput.Security;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+
+
 
 
 /*Name: Nolubabalo Ndongeni
@@ -19,70 +22,62 @@ SecurityConfig.java
 27 September 2022
 */
 
-@Configuration
+
+
+
+
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-   /* @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        //super.configure(http);
-        http
-                .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/patient/save").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/patient/readPatient").hasRole("USER")
-                .antMatchers(HttpMethod.DELETE, "/patient/deletePatient").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/patient/getPatients").hasRole("USER")
-                .and()
-                .httpBasic()
-                .csrf().disable()
-                .formLogin().disable();*/
+@EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true,jsr250Enabled = true)
+public class SecurityConfig  {
 
-        @Override
-        protected void configure(HttpSecurity httpSecurity) throws Exception {
-            httpSecurity.csrf().disable()
-                    .authorizeRequests().antMatchers(HttpMethod.POST, "/testPatient/save").hasRole("ADMIN")
-                    .antMatchers(HttpMethod.GET, "/testPatient/getTestPatient").hasRole("USER")
-                    .antMatchers(HttpMethod.DELETE, "/testPatient/deleteTestPatient").hasRole("ADMIN")
-                    .antMatchers(HttpMethod.GET, "/testPatient/getTestPatients").hasRole("USER")
-                    .anyRequest().authenticated()
-                    .and().httpBasic();
+    @Bean
+    public UserDetailsService users() {
 
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user")
+                .password(passwordEncoder().encode("12345"))
+                .roles("USER")
+                .build());
 
+        manager.createUser(User.withUsername("admin")
+                .password(passwordEncoder().encode("54321"))
+                .roles("USER","ADMIN")
+                .build());
 
+        return manager;
 
-        /*http.httpBasic()
-                .add()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/testPatient/save").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/testPatient/getTestPatient").hasRole("USER")
-                .antMatchers(HttpMethod.DELETE, "/testPatient/deleteTestPatient").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/testPatient/getTestPatients").hasRole("USER")
-                .add()
-                .csrf().disable()
-                .formLogin().disable();*/
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder authentication)
-            throws Exception
-    {
-        authentication.inMemoryAuthentication()
-                .withUser("admin")
-                .password(passwordEncoder().encode("nimda"))
-                .authorities("ROLE_USER");
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+
+                //patient authentication
+                .antMatchers(HttpMethod.POST,"/patient/save").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE,"/patient/deletePatient/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/patient/readPatient").hasRole("USER")
+                .antMatchers(HttpMethod.GET,"/patient/all/").hasRole("USER")
+
+
+                //testPatient authentication
+                .antMatchers(HttpMethod.POST,"/testPatient/save_testPatient").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/testPatient/readTestPatient/").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE,"/testPatient/deleteTestPatient/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/testPatient/getTestPatients").hasRole("USER")
+
+
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-    /*
-    @Bean
-    public PasswordEncoder encoder(){
-        return newBCryptPasswordEncoder()
-    }
-
-    private PasswordEncoder newBCryptPasswordEncoder() {
-    }*/
-
-
 }
+
